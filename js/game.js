be636fe836ec532ce0c7f010afaf685bf60f476c
Renan Silva <renan.silva@mondial-assistance.com.br>
2020-09-15 17:19:37 -0300

    Create the Snake Class

--- js/game.js
+++ js/game.js
@@ -21,6 +21,19 @@ function init(){
     food = new Food(spawnLocation(), "red")
 }
 
+
+function spawnLocation(){
+    
+    let rows = width / tileSize;
+    let cols = height / tileSize;
+
+    let xPos, yPos;
+    
+    xPos = Math.floor(Math.random() * rows) * tileSize;
+    yPos = Math.floor(Math.random() * cols) * tileSize;
+    
+    return { x: xPos, y: yPos};
+}
 class Food {
 
     constructor(pos, color){
@@ -41,15 +54,93 @@ class Food {
     }
 }
 
-function spawnLocation(){
+class Snake {
+    constructor(pos, color){
+        this.x = pos.x;
+        this.y = pos.y;
+        this.tail = [{ x: pos.x - tileSize, y:pos.y}, { x: pos.x - tileSize * 2, y: pos.y}]
+        this.velX = 1;
+        this.velY = 0;
+        this.color = color;
+    }
 
-    let rows = width / tileSize;
-    let cols = height / tileSize;
+    // Drawing the snake on the canvas
+    draw() {
 
-    let xPos, yPos;
+        //Drawing the head of the snake
+        ctx.beginPath();
+        ctx.rect(this.x, this.y, tileSize, tileSize);
+        ctx.fillStyle = this.color;
+        ctx.fill();
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 3;
+        ctx.stroke();
+        ctx.closePath();
 
-    xPos = Math.floor(Math.random() * rows) * tileSize;
-    yPos = Math.floor(Math.random() * cols) * tileSize;
+        // Drawing the tail of the snake.
+        for (var i=0; i < this.tail.length; i++){
 
-    return { x: xPos, y: yPos};
+            ctx.beginPath();
+            ctx.rect(this.tail[i].x, this.tail[i].y, tileSize, tileSize);
+            ctx.fillStyle = this.color;
+            ctx.fill();
+            ctx.strokeStyle = "black";
+            ctx.stroke() = 3;
+            ctx.closePath();
+
+        }
+    }
+
+    // Moving the snake by updating position
+    move() {
+
+        //Movement of the tail
+        for (var i = this.tail.length -1; i > 0; i--) {
+            
+            this.tail[i] = this.tail[i-1];
+        }
+
+        // Updating the start of the tail to acquire the position of the head
+        if (this.tail.length != 0)
+            this.tail[0] = { x: this.x, y: this.y};
+
+        this.x += this.velX * tileSize;
+        this.y += this.velY * tileSize;
+    }
+
+    // Changing the direction of movement of the snake
+    dir(dirX, dirY){
+        this.velX = dirX;
+        this.velY = dirY;
+    }
+
+    // Determine whether the snake has eaten a piece of food.
+    eat(){
+
+        if(Math.abs(this.x - food.x) < tileSize && Math.abs(this.y - food.y) < tileSize){
+
+            //Adding to the tail
+            this.tail.push({});
+            return true;
+        }
+        return false;
+    }
+
+    // Checking of the snake has died
+    die(){
+        for (var i = 0; i < this.tail.length; i++){
+            if(Math.abs(this.x - this.tail[i].x) < tileSize && Math.abs(this.y - this.tail[i].y) < tileSize){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    border(){
+
+        if(this.x + tileSize > width && this.velX != -1 || this.x < 0 && this.velX != 1)
+            this.x = width - this.x;
+        else if(this.y + tileSize > heigth && this.velY != -1 || this.velY != 1 && this.y < 0)
+            this.y = heigth - this.y;
+    }
 }
